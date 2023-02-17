24c0cbd3bf645fa3f532ad8286a6d3ccaf9b1fa6
Zyflx <100444522+Zyflx@users.noreply.github.com>
2023-02-17 16:25:13 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -8,10 +8,13 @@ class Main extends Sprite
 	var gameWidth:Int = 1280; // Game Width in pixels
 	var gameHeight:Int = 720; // Game Height in pixels
 	var framerate:Int = 60; // Game framerate
+	var initialState:Class<FlxState> = TitleScreen; // Initial State of the game
+	var skipSplashScreen:Bool = false; // Toggle to true if you want to skip the flixel splash screen
+	var startFullScreen:Bool = false; // Toggle to true if you want to start the game in fullscreen
 
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(gameWidth, gameHeight, PlayState, framerate, framerate, false, false));
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, framerate, framerate, skipSplashScreen, startFullScreen));
 	}
 }
