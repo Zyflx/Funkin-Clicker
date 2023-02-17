f58b855d4c545e6f1683c6ca1663da91c949f11a
Zyflx <100444522+Zyflx@users.noreply.github.com>
2023-02-17 18:43:15 -0500

    Really bad placeholder title screen

--- source/TitleScreen.hx
+++ source/TitleScreen.hx
@@ -7,10 +7,27 @@ class TitleScreen extends FlxState
     override public function create()
     {
         super.create();
+        
+        bg = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.CYAN);
+        add(bg);
+
+        titleText = new FlxText(0, FlxG.height / 2 - 100, 0, "Funkin' Clicker", 20);
+        titleText.setFormat(FunkPaths.grabFont('vcr.ttf'), 70, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        titleText.borderSize = 1.2;
+        titleText.screenCenter(X);
+        add(titleText);
+
+        startButton = new FlxSprite(0, titleText.y + 100).loadGraphic(FunkPaths.grabImage('start'));
+        startButton.screenCenter(X);
+        add(startButton);
     }
     
     override public function update(elapsed:Float)
     {
         super.update(elapsed);
+        
+         if (FlxG.mouse.overlaps(startButton) && FlxG.mouse.justPressed) {
+            FlxG.switchState(new PlayState());
+        }
     }
 }
